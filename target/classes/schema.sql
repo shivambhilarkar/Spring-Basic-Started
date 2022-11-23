a27a4204b5f065b4688338d9aa5aa1ca34d72257
Shivam Bhilarkar <shivambhilarkar@gmail.com>
2022-11-23 09:08:43 +0530

    Delete target directory

--- target/classes/schema.sql
+++
@@ -1,7 +0,0 @@
-create table if not exists STUDENTDATABASE(
-    id int not null AUTO_INCREMENT,
-    fname varchar(25) not null,
-    lname varchar(25) not null,
-    email varchar(225) not null,
-    phone varchar(12) not null
-);
